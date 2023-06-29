ccc0183629fe05d484eff89b8f855236f01cae76
Basza <basiakedz@gmail.com>
2023-06-29 16:15:25 +0200

    added disabled and loading attribute to button

--- App.js
+++ App.js
@@ -2,12 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
   fetchData();
 });
 
-document.getElementById('fetch-button').addEventListener('click', function() {
-    fetchData();
-})
+document.getElementById("fetch-button").addEventListener("click", function () {
+  fetchData();
+});
 
 function fetchData() {
-    fetch("https://api.adviceslip.com/advice")
+  const diceElement = document.getElementById("dice");
+
+  diceElement.classList.add("loading");
+
+  const fetchButton = document.getElementById('fetch-button')
+  fetchButton.disabled = true;
+
+  fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
     .then((data) => {
       const advice = data.slip.advice;
@@ -19,16 +26,23 @@ function fetchData() {
       const newAdvice = `ADVICE #${adviceNumber}`;
       const newSentence = `“${advice}”`;
 
-      if(adviceElement.textContent === newAdvice || sentenceElement.textContent === newSentence) {
-        fetchData()
+      if (
+        adviceElement.textContent === newAdvice ||
+        sentenceElement.textContent === newSentence
+      ) {
+        fetchData();
         return;
       }
 
       adviceElement.textContent = newAdvice;
       sentenceElement.textContent = newSentence;
+
+      diceElement.classList.remove("loading");
+      fetchButton.disabled = false;
     })
     .catch((error) => {
       console.log("Wystąpił błąd:", error);
+      diceElement.classList.remove("loading");
+      fetchButton.disabled = false;
     });
 }
-
