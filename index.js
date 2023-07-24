a7864d330eff347822c57c0854a1da09dbe31b5e
Basza <basiakedz@gmail.com>
2023-07-24 19:06:40 +0200

    refactored css class names to BEM convention

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ document.getElementById("fetch-button").addEventListener("click", function () {
 function fetchData() {
   const diceElement = document.getElementById("dice");
 
-  diceElement.classList.add("loading");
+  diceElement.classList.add("button__loading");
 
-  const fetchButton = document.getElementById('fetch-button')
+  const fetchButton = document.getElementById("fetch-button");
   fetchButton.disabled = true;
 
   fetch("https://api.adviceslip.com/advice")
@@ -21,28 +21,28 @@ function fetchData() {
       const adviceNumber = data.slip.id;
 
       const adviceElement = document.querySelector("#advice");
-      const sentenceElement = document.querySelector("#sentence");
+      const quotesElement = document.querySelector("#quotes");
 
       const newAdvice = `ADVICE #${adviceNumber}`;
-      const newSentence = `“${advice}”`;
+      const newQuotes = `“${advice}”`;
 
       if (
         adviceElement.textContent === newAdvice ||
-        sentenceElement.textContent === newSentence
+        quotesElement.textContent === newQuotes
       ) {
         fetchData();
         return;
       }
 
       adviceElement.textContent = newAdvice;
-      sentenceElement.textContent = newSentence;
+      quotesElement.textContent = newQuotes;
 
-      diceElement.classList.remove("loading");
+      diceElement.classList.remove("button__loading");
       fetchButton.disabled = false;
     })
     .catch((error) => {
       console.log("Wystąpił błąd:", error);
-      diceElement.classList.remove("loading");
+      diceElement.classList.remove("button__loading");
       fetchButton.disabled = false;
     });
 }
